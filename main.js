55c275a5a3e0a40293feb571a378e0b76cbff0d6
Grzegorz Radomski <rc36@wp.pl>
2018-02-16 13:32:46 +0100

    if/else condition added

--- main.js
+++ main.js
@@ -1,38 +1,6 @@
-2 + 2;
-
-//alert("hello");
-//confirm("Good");
-//prompt("milego dnia");
-
-2/2;
-
-//now create new row for marge repo
-
-var a = 300
-undefined
-var b = 2
-undefined
-a/b
-150
-c = "hello"
-"hello"
-d = "world"
-"world"
-c+d
-"helloworld"
-c + ""+d
-"helloworld"
-c + " "+d
-"hello world"
-c + " +"+d
-"hello +world"
-c + " +"+d
-"hello +world"
-6<2
-false
-6>2
-true
-3+6
-9
-3 + "6"
-"36"
+var myWife = prompt("what is your favurite colour?")
+if (myWife === "red") {
+    alert("very good!")
+}else{
+        alert("you mistake")
+    }
