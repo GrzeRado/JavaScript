ea5fe0bcfa1f500a98a35ac1b92618c4d96933d9
Grzegorz Radomski <rc36@wp.pl>
2018-02-17 06:36:07 +0100

    add variable

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-
-var myColour = prompt("what is your favurite colour?")
+/* var myColour = prompt("what is your favurite colour?")
 if (myColour === "red") {
     alert("very good!")
 }else{
         alert("you mistake")
-    }
+    }*/
+
+var number = Math.random();
+alert(number);
