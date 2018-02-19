10d24824affe6d8becc48eb702eb1c5d4d93f6cd
Grzegorz Radomski <rc36@wp.pl>
2018-02-19 17:58:42 +0100

    Simple Game Alien Attack

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+alert("This is alien attack. You must find the door and run away!!!");
+var weapon = prompt("machine gun or laser?");
+var randomNumber = Math.random(Math.random());
+
+alert("You attack alien with" + " " + weapon);
+
+if(randomNumber === 0){
+    alert("Alien kill you. You die!");
+}else if (randomNumber === 1){
+    alert("You kill the alien" + " " + weapon +".You win!");
+}
+
 /* var myColour = prompt("what is your favurite colour?")
 if (myColour === "red") {
     alert("very good!")
@@ -5,15 +17,15 @@ if (myColour === "red") {
         alert("you mistake")
     }*/
 
-conditions below added
+//conditions below added
 /*var number = Math.random();
 alert(number);
 */
 
-var number = Math.round( Math.random() * 3);
+//var number = Math.round( Math.random() * 3);
 //alert(number);
 
-var number = 0;
+/* var number = 0;
 alert(number);
 
 if(number === 3 && number != 0){
@@ -21,3 +33,4 @@ if(number === 3 && number != 0){
 }else{
     alert("numer is less than 3")
 }
+*/
